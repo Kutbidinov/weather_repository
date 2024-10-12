452c400058b8f5bee32ef7536918c7a286669d04
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-10-12 21:11:27 +0600

    !

--- weather/views.py
+++ weather/views.py
@@ -1,42 +1,46 @@
 import requests
 from django.shortcuts import render
 from .models import City
+from .forms import CityForm
+
 
 def index(request):
-
     appid = 'baa4b71c8f92b88bb68e5b40a9d17e66'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&units=metric&appid=' + appid
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city_name = form.cleaned_data['name']
+            # Проверка на наличие города в базе данных
+            if not City.objects.filter(name=city_name).exists():
+                form.save()
 
-
+    form = CityForm()
     cities = City.objects.all()
 
     all_cities = []
 
-
     for city in cities:
-        res = requests.get(url.format(cities)).json()
+        res = requests.get(url.format(city.name)).json()  # Исправил 'cities' на 'city.name'
+        print(res)
         if 'main' in res and 'weather' in res:
             city_info = {
-                'city': cities.name,
+                'city': city.name,
                 'temp': res['main'].get('temp', 'Неизвестная температура'),
                 'icon': res['weather'][0].get('icon', 'нет иконки'),
             }
         else:
             city_info = {
-                'city': cities,
+                'city': city.name,
                 'error': 'Не удалось получить данные о погоде',
             }
 
-
         all_cities.append(city_info)
 
-
-
     context = {
-
-        'all_info': all_cities}
-
-
+        'all_info': all_cities,
+        'form': form
+    }
 
     return render(request, 'weather/index.html', context)
