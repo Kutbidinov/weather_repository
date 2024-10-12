87a96ee1a8723d6180e9206c5cc5c48d365612d4
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-10-12 15:19:15 +0600

    !

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,42 @@
+import requests
 from django.shortcuts import render
+from .models import City
 
 def index(request):
-    return render(request, 'weather/index.html')
+
+    appid = 'baa4b71c8f92b88bb68e5b40a9d17e66'
+
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&units=metric&appid=' + appid
+
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+
+    for city in cities:
+        res = requests.get(url.format(cities)).json()
+        if 'main' in res and 'weather' in res:
+            city_info = {
+                'city': cities.name,
+                'temp': res['main'].get('temp', 'Неизвестная температура'),
+                'icon': res['weather'][0].get('icon', 'нет иконки'),
+            }
+        else:
+            city_info = {
+                'city': cities,
+                'error': 'Не удалось получить данные о погоде',
+            }
+
+
+        all_cities.append(city_info)
+
+
+
+    context = {
+
+        'all_info': all_cities}
+
+
+
+    return render(request, 'weather/index.html', context)
